a771f0edf20240ba08012b57c453c136d650a08b
dmuromkin <muromkin1997@gmail.com>
2019-03-11 12:16:14 +0300

    Logging in files fixed

--- homeworks/HW2/src/main/java/Main.java
+++ homeworks/HW2/src/main/java/Main.java
@@ -39,7 +39,7 @@ public class Main {
 
     public static void main(String[] args) {
         int count = 0;
-        log.info("Game started!");
+        log.info("Game started!");//logger
         File path = new File("dictionary.txt");
         if (!path.exists())
             System.out.println("File not found");

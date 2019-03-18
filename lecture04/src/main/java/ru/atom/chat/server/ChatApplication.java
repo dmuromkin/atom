3e16dbed92eac05d1310d3785db8f79cbd26b512
dmuromkin <muromkin1997@gmail.com>
2019-03-18 12:18:21 +0300

    First commit

--- lecture04/src/main/java/ru/atom/chat/server/ChatApplication.java
+++ lecture04/src/main/java/ru/atom/chat/server/ChatApplication.java
@@ -7,5 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChatApplication {
     public static void main(String[] args) {
         SpringApplication.run(ChatApplication.class, args);
-    }
+    }//first commit
 }
